a67d26dcc69184e178a55b71057a4a25ae7187b8
japtar10101 <japtar10101@localhost>
2010-09-04 15:12:16 -0400

    Initial commit.

---
+++ src/com/games/deception/Deception.java
@@ -0,0 +1,71 @@
+package com.games.deception;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+public class Deception extends BaseGameActivity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_HEIGHT = 800;
+	private static final int CAMERA_WIDTH = 480;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private Camera mCamera;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.PORTRAIT, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera));
+	}
+
+	@Override
+	public void onLoadResources() {
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene(1);
+		scene.setBackground(new ColorBackground(0, 0, 0.8784f));
+
+		return scene;
+	}
+
+	@Override
+	public void onLoadComplete() {
+
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
